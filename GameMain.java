1feda3e688ff8d4bfd43b941d39c5f42bca52cbf
SomethingFamilyFriendly <83571918+SomethingFamilyFriendly@users.noreply.github.com>
2021-05-05 20:22:24 +1200

    Update GameMain.java
    Made it more user friendly

--- GameMain.java
+++ GameMain.java
@@ -93,13 +93,13 @@ public class GameMain extends JPanel implements MouseListener{
 			}       
 			} else if (currentState == GameState.Draw) {          
 				statusBar.setForeground(Color.RED);          
-				statusBar.setText("It's a Draw! Click to play again.");       
+				statusBar.setText("It's a Draw! Click to play again. Or close the window to exit.");       
 			} else if (currentState == GameState.Cross_won) {          
 				statusBar.setForeground(Color.RED);          
-				statusBar.setText("'X' Won! Click to play again.");       
+				statusBar.setText("'X' Won! Click to play again. Or close the window to exit.");       
 			} else if (currentState == GameState.Nought_won) {          
 				statusBar.setForeground(Color.RED);          
-				statusBar.setText("'O' Won! Click to play again.");       
+				statusBar.setText("'O' Won! Click to play again. Or close the window to exit.");       
 			}
 		}
 		
